b3674b5104bf633368e26288938e97595b2bb4c5
remykarem <raimi.bkarim@gmail.com>
2021-08-17 00:23:25 +0800

    day04 refaktor

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -4,7 +4,7 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-const DELIMITER: &'static str = ":";
+const DELIMITER: &str = ":";
 lazy_static! {
     static ref ALL_FIELDS: HashSet<&'static str> =
         ["byr", "eyr", "iyr", "hgt", "pid", "hcl", "ecl"]
@@ -16,22 +16,20 @@ lazy_static! {
 pub fn main() {
     let mut num_valid = 0;
 
-    let file = File::open("./data/day04.txt").expect("cannot open file");
+    let file = File::open("./src/input.txt").expect("cannot open file");
     let reader = BufReader::new(file);
 
     let mut passport = String::from("");
-    reader.lines().for_each(|line| {
-        let l = line.unwrap();
-
-        if l == "" {
+    reader.lines().map(Result::unwrap).for_each(|line| {
+        if line.is_empty() {
             // Process the buffer
             if check_passport2(&passport) {
                 num_valid += 1;
             }
             passport.clear();
         } else {
-            passport.push_str(" ");
-            passport.push_str(&l);
+            passport.push(' ');
+            passport.push_str(&line);
         }
     });
 
@@ -86,7 +84,7 @@ fn check_passport2(passport: &str) -> bool {
 
 fn validate_byr(input: &str) -> bool {
     let year = input.parse::<i32>().unwrap();
-    year >= 1920 && year <= 2002
+    (1920..=2002).contains(&year)
 }
 fn validate_iyr(input: &str) -> bool {
     let year = input.parse::<i32>().unwrap();
@@ -133,142 +131,109 @@ mod tests {
 
     #[test]
     fn validating_byr() {
-        assert_eq!(validate_byr("1920"), true);
-        assert_eq!(validate_byr("2002"), true);
-        assert_eq!(validate_byr("2000"), true);
-        assert_eq!(validate_byr("2020"), false);
+        assert!(validate_byr("1920"));
+        assert!(validate_byr("2002"));
+        assert!(validate_byr("2000"));
+        assert!(!validate_byr("2020"));
     }
 
     #[test]
     fn validating_iyr() {
-        assert_eq!(validate_iyr("2010"), true);
-        assert_eq!(validate_iyr("2020"), true);
-        assert_eq!(validate_iyr("2015"), true);
-        assert_eq!(validate_iyr("2030"), false);
+        assert!(validate_iyr("2010"));
+        assert!(validate_iyr("2020"));
+        assert!(validate_iyr("2015"));
+        assert!(!validate_iyr("2030"));
     }
 
     #[test]
     fn validating_eyr() {
-        assert_eq!(validate_eyr("2020"), true);
-        assert_eq!(validate_eyr("2030"), true);
-        assert_eq!(validate_eyr("2025"), true);
-        assert_eq!(validate_eyr("2050"), false);
+        assert!(validate_eyr("2020"));
+        assert!(validate_eyr("2030"));
+        assert!(validate_eyr("2025"));
+        assert!(!validate_eyr("2050"));
     }
 
     #[test]
     fn validating_hgt() {
-        assert_eq!(validate_hgt("150cm"), true);
-        assert_eq!(validate_hgt("193cm"), true);
-        assert_eq!(validate_hgt("180cm"), true);
-        assert_eq!(validate_hgt("76in"), true);
-        assert_eq!(validate_hgt("59in"), true);
-        assert_eq!(validate_hgt("gibberish"), false);
+        assert!(validate_hgt("150cm"));
+        assert!(validate_hgt("193cm"));
+        assert!(validate_hgt("180cm"));
+        assert!(validate_hgt("76in"));
+        assert!(validate_hgt("59in"));
+        assert!(!validate_hgt("gibberish"));
     }
 
     #[test]
     fn validating_pid() {
-        assert_eq!(validate_pid("123456789"), true);
-        assert_eq!(validate_pid("123"), false);
+        assert!(validate_pid("123456789"));
+        assert!(!validate_pid("123"));
     }
 
     #[test]
     fn validating_hcl() {
-        assert_eq!(validate_hcl("#003366"), true);
-        assert_eq!(validate_hcl("#00f14e"), true);
-        assert_eq!(validate_hcl("#1234567"), false);
-        assert_eq!(validate_hcl("1234567"), false);
-        assert_eq!(validate_hcl("#gggggg"), false);
+        assert!(validate_hcl("#003366"));
+        assert!(validate_hcl("#00f14e"));
+        assert!(!validate_hcl("#1234567"));
+        assert!(!validate_hcl("1234567"));
+        assert!(!validate_hcl("#gggggg"));
     }
 
     #[test]
     fn validating_ecl() {
-        assert_eq!(validate_ecl("amb"), true);
-        assert_eq!(validate_ecl("AMB"), false);
+        assert!(validate_ecl("amb"));
+        assert!(!validate_ecl("AMB"));
     }
 
     #[test]
     fn check_passports2() {
-        assert_eq!(
-            check_passport2(
-                "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
-            ),
-            true
-        );
-        assert_eq!(
-            check_passport2(
-                r#"eyr:2029 ecl:blu cid:129 byr:1989
+        assert!(check_passport2(
+            "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+        ));
+        assert!(check_passport2(
+            r#"eyr:2029 ecl:blu cid:129 byr:1989
     iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm"#
-            ),
-            true
-        );
-        assert_eq!(
-            check_passport2(
-                r#"pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+        ));
+        assert!(check_passport2(
+            r#"pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
     hcl:#623a2f"#
-            ),
-            true
-        );
+        ));
 
-        assert_eq!(
-            check_passport2(
-                r#"eyr:1972 cid:100
+        assert!(!check_passport2(
+            r#"eyr:1972 cid:100
     hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926"#
-            ),
-            false
-        );
-        assert_eq!(
-            check_passport2(
-                r#"iyr:2019
+        ));
+        assert!(!check_passport2(
+            r#"iyr:2019
     hcl:#602927 eyr:1967 hgt:170cm
     ecl:grn pid:012533040 byr:1946"#
-            ),
-            false
-        );
-        assert_eq!(
-            check_passport2(
-                r#"hcl:dab227 iyr:2012
+        ));
+        assert!(!check_passport2(
+            r#"hcl:dab227 iyr:2012
     ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277"#
-            ),
-            false
-        );
-        assert_eq!(
-            check_passport2(
-                r#"hgt:59cm ecl:zzz
+        ));
+        assert!(!check_passport2(
+            r#"hgt:59cm ecl:zzz
     eyr:2038 hcl:74454a iyr:2023
     pid:3556412378 byr:2007"#
-            ),
-            false
-        );
+        ));
 
-        assert_eq!(
-            check_passport(
-                r#"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+        assert!(check_passport(
+            r#"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
         byr:1937 iyr:2017 cid:147 hgt:183cm"#
-            ),
-            true
-        );
-        assert_eq!(
-            check_passport(
-                r#"iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+        ));
+        assert!(!check_passport(
+            r#"iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
         hcl:#cfa07d byr:1929"#
-            ),
-            false
-        );
-        assert_eq!(
-            check_passport(
-                r#"hcl:#ae17e1 iyr:2013
+        ));
+        assert!(check_passport(
+            r#"hcl:#ae17e1 iyr:2013
                 eyr:2024
                 ecl:brn pid:760753108 byr:1931
                 hgt:179cm"#
-            ),
-            true
-        );
-        assert_eq!(
-            check_passport(
-                r#"hcl:#cfa07d eyr:2025 pid:166559648
+        ));
+        assert!(!check_passport(
+            r#"hcl:#cfa07d eyr:2025 pid:166559648
                 iyr:2011 ecl:brn hgt:59in"#,
-            ),
-            false
-        );
+        ));
     }
 }
---
+++ day04/src/stuff.md
@@ -0,0 +1,5 @@
+`assert_eq!` vs `assert!`
+
+`year >= 1920 && year <= 2002`
+
+`(1920..=2002).contains(&year)`

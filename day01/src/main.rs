4ca37506c50af764828fa8dbcd9d1ea151e0be9e
remykarem <raimi.bkarim@gmail.com>
2021-08-20 21:26:50 +0800

    macro_rules!

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,6 +2,17 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+// https://riptutorial.com/rust/example/4149/create-a-hashset-macro
+macro_rules! set {
+    ($($x:expr),*) => {
+        {
+            let mut tmp = HashSet::new();
+            $(tmp.insert($x);)*
+            tmp
+        }
+    };
+}
+
 fn main() {
     let file = File::open("./src/input.txt").expect("file not found");
     let reader = BufReader::new(file);
@@ -33,7 +44,7 @@ fn two_sum(sum: i32, nums: &[i32]) -> Option<HashSet<i32>> {
     for &num in nums {
         let target = sum - num;
         if candidates.contains(&target) {
-            return Some([num, target].iter().cloned().collect());
+            return Some(set![num, target]);
         } else {
             candidates.insert(num);
         }
@@ -53,7 +64,7 @@ fn three_sum(sum: i32, nums: &[i32]) -> Option<HashSet<i32>> {
         for &num in nums[(i + 1)..len].iter() {
             let target = sum - context - num;
             if candidates.contains(&target) {
-                return Some([num, context, target].iter().cloned().collect());
+                return Some(set![num, context, target]);
             } else {
                 candidates.insert(num);
             }
@@ -68,17 +79,11 @@ mod tests {
 
     #[test]
     fn test_three_sum() {
-        assert_eq!(
-            three_sum(14, &[1, 9, 4, 3]),
-            Some([1, 9, 4].iter().cloned().collect())
-        );
+        assert_eq!(three_sum(14, &[1, 9, 4, 3]), Some(set![1, 9, 4]));
     }
 
     #[test]
     fn test_two_sum() {
-        assert_eq!(
-            two_sum(13, &[1, 9, 4, 3]),
-            Some([9, 4].iter().cloned().collect())
-        );
+        assert_eq!(two_sum(13, &[1, 9, 4, 3]), Some(set![9, 4]));
     }
 }

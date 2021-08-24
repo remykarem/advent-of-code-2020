da3194ec0e8b32ba7c51d32e5710d760ed6d6ec1
remykarem <raimi.bkarim@gmail.com>
2021-08-25 07:41:55 +0800

    Day12 AM

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,5 +1,23 @@
+use std::collections::VecDeque;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 use crate::Direction::*;
 use crate::Turn::*;
+
+fn main() {
+    let file = File::open("./src/input.txt").expect("Could not open file");
+    let reader = BufReader::new(file);
+
+    let mut ship = Ship::new(East);
+
+    reader.lines().map(Result::unwrap).for_each(|line| {
+        ship.parse_command(line);
+    });
+    println!("{}", ship.get_manhattan_distance());
+
+    println!("Hello, world!");
+}
 
 #[derive(PartialEq)]
 enum Turn {
@@ -14,35 +32,15 @@ enum Direction {
     East,
     West,
 }
-impl Direction {
-    fn adjust(&mut self, turn: Turn, degrees: u32) {
-        let direction = self;
-        if turn == Right {
-            for _ in 0..degrees / 90 {
-                *direction = match direction {
-                    Direction::North => Direction::East,
-                    Direction::East => Direction::South,
-                    Direction::South => Direction::West,
-                    Direction::West => Direction::North,
-                };
-            }
-        } else {
-            for _ in 0..degrees / 90 {
-                *direction = match direction {
-                    Direction::North => Direction::West,
-                    Direction::East => Direction::North,
-                    Direction::South => Direction::East,
-                    Direction::West => Direction::South,
-                };
-            }
-        }
-    }
-}
 
 struct Ship {
     x: i32,
     y: i32,
-    facing: Direction,
+    bearing: VecDeque<Direction>,
+}
+struct Waypoint {
+    x: i32,
+    y: i32,
 }
 impl Ship {
     fn mv(&mut self, direction: Direction, dist: i32) {
@@ -54,35 +52,66 @@ impl Ship {
         }
     }
     fn fwd(&mut self, dist: i32) {
-        match self.facing {
+        match self.is_facing() {
             Direction::North => self.y += dist,
             Direction::South => self.y -= dist,
             Direction::East => self.x += dist,
             Direction::West => self.x -= dist,
         }
     }
+    fn is_facing(&self) -> &Direction {
+        &self.bearing[0]
+    }
     fn turn(&mut self, turn: Turn, degrees: u32) {
         if degrees % 90 != 0 {
             panic!("degrees must be a multiple of 90");
         };
-        self.facing.adjust(turn, degrees);
+        if turn == Right {
+            self.bearing.rotate_left((degrees / 90) as usize);
+        } else {
+            self.bearing.rotate_right((degrees / 90) as usize);
+        }
     }
     fn new(facing: Direction) -> Ship {
+        let mut bearing = VecDeque::new();
+        bearing.push_back(North);
+        bearing.push_back(East);
+        bearing.push_back(South);
+        bearing.push_back(West);
+
+        let mut direction = &bearing[0];
+        while *direction != facing {
+            bearing.rotate_left(1);
+            direction = &bearing[0];
+        }
         Ship {
             x: 0,
             y: 0,
-            facing,
+            bearing,
         }
     }
     fn get_pos(&self) -> (i32, i32) {
         (self.x, self.y)
     }
-    fn execute(self, command: &str) {
+    fn get_manhattan_distance(&self) -> i32 {
+        self.x.abs() + self.y.abs()
     }
-}
 
-fn main() {
-    println!("Hello, world!");
+    fn parse_command(&mut self, line: String) {
+        let g = &line[0..1];
+        let h = str::parse::<i32>(&line[1..line.len()]).unwrap();
+
+        match g {
+            "N" => self.mv(North, h),
+            "S" => self.mv(South, h),
+            "E" => self.mv(East, h),
+            "W" => self.mv(West, h),
+            "L" => self.turn(Left, h as u32),
+            "R" => self.turn(Right, h as u32),
+            "F" => self.fwd(h),
+            _ => panic!("Invalid command"),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -120,36 +149,51 @@ mod tests {
     fn test_turn_1() {
         let mut ship = Ship::new(North);
         ship.turn(Right, 0);
-        assert_eq!(ship.facing, North);
+        assert_eq!(ship.is_facing(), &North);
     }
     #[test]
     fn test_turn_2() {
         let mut ship = Ship::new(North);
         ship.turn(Right, 90);
-        assert_eq!(ship.facing, East);
+        assert_eq!(ship.is_facing(), &East);
     }
     #[test]
     fn test_turn_3() {
         let mut ship = Ship::new(North);
         ship.turn(Right, 180);
-        assert_eq!(ship.facing, South);
+        assert_eq!(ship.is_facing(), &South);
     }
     #[test]
     fn test_turn_4() {
         let mut ship = Ship::new(North);
         ship.turn(Right, 270);
-        assert_eq!(ship.facing, West);
+        assert_eq!(ship.is_facing(), &West);
     }
     #[test]
     fn test_turn_5() {
         let mut ship = Ship::new(North);
         ship.turn(Right, 360);
-        assert_eq!(ship.facing, North);
+        assert_eq!(ship.is_facing(), &North);
     }
     #[test]
     #[should_panic]
     fn test_turn_6() {
         let mut ship = Ship::new(North);
         ship.turn(Right, 99);
+    }
+    #[test]
+    fn test_case() {
+        let mut ship = Ship::new(East);
+        ship.fwd(10);
+        assert_eq!(ship.get_pos(), (10, 0));
+        ship.mv(North, 3);
+        assert_eq!(ship.get_pos(), (10, 3));
+        ship.fwd(7);
+        assert_eq!(ship.get_pos(), (17, 3));
+        ship.turn(Right, 90);
+        assert_eq!(ship.get_pos(), (17, 3));
+        ship.fwd(11);
+        assert_eq!(ship.get_pos(), (17, -8));
+        assert_eq!(ship.get_manhattan_distance(), 25);
     }
 }

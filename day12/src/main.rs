c15bb3e58949a71a8569820bbf4fb50ff05ffce7
remykarem <raimi.bkarim@gmail.com>
2021-08-25 23:29:27 +0800

    I'm calling it a Day

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -10,11 +10,19 @@ fn main() {
     let reader = BufReader::new(file);
 
     let mut ship = Ship::new(East);
+    let mut ship2 = Ship2::new(10, 1);
 
     reader.lines().map(Result::unwrap).for_each(|line| {
         ship.parse_command(line);
     });
-    println!("{}", ship.get_manhattan_distance());
+    println!("Part 1: {}", ship.get_manhattan_distance());
+
+    let file = File::open("./src/input.txt").expect("Could not open file");
+    let reader = BufReader::new(file);
+    reader.lines().map(Result::unwrap).for_each(|line| {
+        ship2.parse_command(line);
+    });
+    println!("Part 2: {}", ship2.get_manhattan_distance());
 
     println!("Hello, world!");
 }
@@ -38,9 +46,95 @@ struct Ship {
     y: i32,
     bearing: VecDeque<Direction>,
 }
-struct Waypoint {
+struct Ship2 {
     x: i32,
     y: i32,
+    waypoint_offset: WaypointOffset,
+}
+struct WaypointOffset {
+    x: i32,
+    y: i32,
+    bearing: VecDeque<i32>,
+}
+impl WaypointOffset {
+    fn rotate(&mut self, turn: Turn, degrees: u32) {
+        self.update();
+        if degrees % 90 != 0 {
+            panic!("degrees must be a multiple of 90");
+        };
+        if turn == Right {
+            self.bearing.rotate_left((degrees / 90) as usize);
+        } else {
+            self.bearing.rotate_right((degrees / 90) as usize);
+        }
+        self.x = self.bearing[0];
+        self.y = self.bearing[1];
+    }
+    fn get_offset(&self) -> (i32, i32) {
+        (self.x, self.y)
+    }
+    fn new(x: i32, y: i32) -> WaypointOffset {
+        let mut bearing = VecDeque::new();
+        WaypointOffset { x, y, bearing }
+    }
+    fn update(&mut self) {
+        let (x, y) = (self.x, self.y);
+        self.bearing.clear();
+        self.bearing.push_back(x);
+        self.bearing.push_back(y);
+        self.bearing.push_back(-x);
+        self.bearing.push_back(-y);
+    }
+}
+impl Ship2 {
+    fn mv(&mut self, direction: Direction, dist: i32) {
+        match direction {
+            Direction::North => self.waypoint_offset.y += dist,
+            Direction::South => self.waypoint_offset.y -= dist,
+            Direction::East => self.waypoint_offset.x += dist,
+            Direction::West => self.waypoint_offset.x -= dist,
+        }
+    }
+    fn fwd(&mut self, times: i32) {
+        self.x += times * self.waypoint_offset.x;
+        self.y += times * self.waypoint_offset.y;
+    }
+    fn new(x: i32, y: i32) -> Ship2 {
+        let waypoint_offset = WaypointOffset::new(x, y);
+        Ship2 {
+            x: 0,
+            y: 0,
+            waypoint_offset,
+        }
+    }
+    fn get_pos(&self) -> (i32, i32) {
+        (self.x, self.y)
+    }
+    fn rotate_waypoint(&mut self, turn: Turn, degrees: u32) {
+        self.waypoint_offset.rotate(turn, degrees);
+    }
+    fn get_manhattan_distance(&self) -> i32 {
+        self.x.abs() + self.y.abs()
+    }
+    fn parse_command(&mut self, line: String) {
+        let g = &line[0..1];
+        let h = str::parse::<i32>(&line[1..line.len()]).unwrap();
+
+        match g {
+            "N" => self.mv(North, h),
+            "S" => self.mv(South, h),
+            "E" => self.mv(East, h),
+            "W" => self.mv(West, h),
+            "L" => self.rotate_waypoint(Left, h as u32),
+            "R" => self.rotate_waypoint(Right, h as u32),
+            "F" => self.fwd(h),
+            _ => panic!("Invalid command"),
+        }
+    }
+    //     10   4
+    //      4 -10
+    //    -10  -4
+    //     -4  10
 }
 impl Ship {
     fn mv(&mut self, direction: Direction, dist: i32) {
@@ -195,5 +289,33 @@ mod tests {
         ship.fwd(11);
         assert_eq!(ship.get_pos(), (17, -8));
         assert_eq!(ship.get_manhattan_distance(), 25);
+    }
+    #[test]
+    fn test_case2() {
+        let mut ship = Ship2::new(10, 1);
+        assert_eq!(ship.get_pos(), (0, 0));
+        assert_eq!(ship.waypoint_offset.get_offset(), (10, 1));
+
+        ship.fwd(10);
+        assert_eq!(ship.get_pos(), (100, 10));
+        assert_eq!(ship.waypoint_offset.get_offset(), (10, 1));
+
+        ship.mv(North, 3);
+        assert_eq!(ship.get_pos(), (100, 10));
+        assert_eq!(ship.waypoint_offset.get_offset(), (10, 4));
+
+        ship.fwd(7);
+        assert_eq!(ship.get_pos(), (170, 38));
+        assert_eq!(ship.waypoint_offset.get_offset(), (10, 4));
+        
+        ship.rotate_waypoint(Right,  90);
+        assert_eq!(ship.get_pos(), (170, 38));
+        assert_eq!(ship.waypoint_offset.get_offset(), (4, -10));
+
+        ship.fwd(11);
+        assert_eq!(ship.get_pos(), (214, -72));
+        assert_eq!(ship.waypoint_offset.get_offset(), (4, -10));
+
+        assert_eq!(ship.get_manhattan_distance(), 286);
     }
 }

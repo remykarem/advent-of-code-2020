3eda69dbb4a66cb1bfdff3551f25dba7b715b3e8
remykarem <raimi.bkarim@gmail.com>
2021-09-16 19:12:42 +0800

    revisit day11

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,19 +1,88 @@
-#[macro_use]
-extern crate ndarray;
+use State::*;
 
-use ndarray::prelude::*;
+fn main() {}
 
-fn main() {
-    let X = array![[1, 2, 3, 4, 5], [1, 2, 3, 4, 5],];
-    println!("{}", X);
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum State {
+    Empty,
+    Floor,
+    Occupied,
+}
 
-    let mut Y = X.clone();
+#[cfg(test)]
+mod tests {
 
-    Y.indexed_iter_mut()
-        .zip(X.indexed_iter())
-        // .for_each(|((pos, y), (_, x))| {
-        //     let (i,j) = pos;
-        //     *y = *x + 1;
-        // });
-    println!("{}", Y);
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let arr = vec![vec![Floor, Empty, Occupied], vec![Floor, Floor, Occupied]];
+
+        assert_eq!(count_adjacent_seats(&arr, 0, 0), 0);
+        assert_eq!(count_adjacent_seats(&arr, 0, 2), 1);
+        assert_eq!(count_adjacent_seats(&arr, 1, 2), 1);
+        assert_eq!(count_adjacent_seats(&arr, 1, 1), 2);
+    }
+
+    #[test]
+    fn it_works2() {
+        let arr = vec![vec![Floor, Empty, Occupied], vec![Floor, Floor, Occupied]];
+        let exp = [[Floor, Empty, Occupied], [Floor, Floor, Occupied]];
+
+        let (obs, changes) = get_next_state(&arr);
+
+        obs[0].iter().zip(exp[0].iter()).for_each(|(a, b)| {
+            assert_eq!(a, b);
+        });
+        obs[1].iter().zip(exp[1].iter()).for_each(|(a, b)| {
+            assert_eq!(a, b);
+        });
+        assert_eq!(changes, 0);
+    }
+}
+
+fn count_adjacent_seats(arr: &[Vec<State>], x: i32, y: i32) -> u32 {
+    let mut sum = 0;
+
+    for i in x - 1..=x + 1 {
+        for j in y - 1..=y + 1 {
+            if (i == x && j == y) || i < 0 || j < 0 || i >= 2 || j >= 3 {
+                continue;
+            } else {
+                match arr[i as usize][j as usize] {
+                    Occupied => sum += 1,
+                    _ => continue,
+                }
+            }
+        }
+    }
+    sum
+}
+
+fn get_next_state(arr: &[Vec<State>]) -> (Vec<Vec<State>>, u32) {
+    let mut new_arr = arr.clone();
+    let mut changes: u32 = 0;
+
+    for i in 0..2 {
+        for j in 0..3 {
+            let sum = count_adjacent_seats(&arr, i as i32, j as i32);
+            match arr[i][j] {
+                Occupied => {
+                    if sum >= 4 {
+                        new_arr[i][j] = Empty;
+                        changes += 1;
+                    }
+                }
+                Empty => {
+                    if sum == 0 {
+                        new_arr[i][j] = Occupied;
+                        changes += 1;
+                    }
+                }
+                _ => continue,
+            }
+        }
+    }
+
+    (new_arr, changes)
 }
